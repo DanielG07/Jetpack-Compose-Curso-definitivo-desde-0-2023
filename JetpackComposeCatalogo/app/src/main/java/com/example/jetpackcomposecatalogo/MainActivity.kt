be91638ef6e0594791828cf602bbdf34ef826390
Daniel González <danielgj7@outlook.com>
2024-07-18 13:32:26 -0600

    Profundización sobre componente Image
    Se revisa algunos atributos del modifier para el componente de Image

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,6 +21,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -40,6 +42,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -65,7 +68,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    MyImage()
+                    MyImageAdvance()
                 }
             }
         }
@@ -76,8 +79,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetpackComposeCatalogoTheme {
-        MyImage()
+        MyImageAdvance()
     }
+}
+
+@Composable
+fun MyImageAdvance() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "ejemplo",
+        modifier =
+            Modifier
+                .clip(RoundedCornerShape(25.dp))
+                .border(5.dp, Color.Red, RoundedCornerShape(25.dp)),
+    )
 }
 
 @Composable
