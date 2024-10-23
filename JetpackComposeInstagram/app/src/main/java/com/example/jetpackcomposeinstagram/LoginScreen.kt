9e2b1a2c23f84e7fcb5a02740dfd974761829286
Daniel González <danielgj7@outlook.com>
2024-10-23 13:23:56 -0600

    Finish instagram login screen

--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/LoginScreen.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomposeinstagram
 
 import android.app.Activity
+import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -107,9 +108,15 @@ fun Body(modifier: Modifier) {
     Column(modifier = modifier) {
         ImageLogo(Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.size(16.dp))
-        Email(email, { email = it })
+        Email(email, {
+            email = it
+            isLoginEnable = enableLogin(email, password)
+        })
         Spacer(modifier = Modifier.size(4.dp))
-        Password(password, { password = it })
+        Password(password, {
+            password = it
+            isLoginEnable = enableLogin(email, password)
+        })
         Spacer(modifier = Modifier.size(8.dp))
         ForgotPassword(Modifier.align(Alignment.End))
         Spacer(modifier = Modifier.size(16.dp))
@@ -183,6 +190,10 @@ fun LoginButton(loginEnable: Boolean) {
     ) {
         Text(text = "Log In")
     }
+}
+
+fun enableLogin(email: String, password: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6
 }
 
 @Composable
