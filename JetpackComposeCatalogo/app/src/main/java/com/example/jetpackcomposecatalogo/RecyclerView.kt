1cbbbd8711adf590ad1ce664dc72a34b6d139b37
Daniel González <danielgj7@outlook.com>
2024-12-05 18:08:02 -0600

    Review of LazyListState

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/RecyclerView.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/RecyclerView.kt
@@ -15,9 +15,15 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +33,45 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecatalogo.model.SuperHero
+import kotlinx.coroutines.launch
+
+@Composable
+fun MySuperHeroSpecialControlView() {
+    val context = LocalContext.current
+    val rvState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        LazyColumn(
+            state = rvState,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.weight(1f),
+        ) {
+            items(getSuperHero()) { superhero ->
+                ItemHero(superHero = superhero) {
+                    Toast.makeText(context, it.superHeroName, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        val showButton by remember {
+            derivedStateOf {
+                rvState.firstVisibleItemIndex > 0
+            }
+        }
+
+        rvState.firstVisibleItemScrollOffset
+
+        if (showButton) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    rvState.animateScrollToItem(0)
+                }
+            }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text(text = "Soy un boton cool")
+            }
+        }
+    }
+}
 
 @Composable
 fun MySimpleRecyclerView() {
