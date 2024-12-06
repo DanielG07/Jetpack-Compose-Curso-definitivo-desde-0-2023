1f14d0e230e9a0e3950d470edbbe214497cf3c36
Daniel González <danielgj7@outlook.com>
2024-12-06 17:02:08 -0600

    Component Scaffold

---
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Scaffold.kt
@@ -0,0 +1,12 @@
+package com.example.jetpackcomposecatalogo
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample() {
+    Scaffold {
+    }
+}
