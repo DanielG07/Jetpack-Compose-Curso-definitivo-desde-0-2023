52923d5415dfcbe0902fa2353b718454081953b0
Daniel González <danielgj7@outlook.com>
2024-12-06 17:42:56 -0600

    Component NavigationBar

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Scaffold.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Scaffold.kt
@@ -2,11 +2,16 @@ package com.example.jetpackcomposecatalogo
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -14,8 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import kotlinx.coroutines.launch
 
@@ -34,6 +42,7 @@ fun ScaffoldExample() {
                 scope.launch { scaffoldHostState.showSnackbar("Has pulsado $it") }
             }
         },
+        bottomBar = { MyBottomNavigation() },
     ) {
     }
 }
@@ -62,4 +71,32 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
             }
         },
     )
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember { mutableStateOf(0) }
+
+    NavigationBar {
+        NavigationBarItem(selected = index == 0, onClick = { index = 0 }, icon = {
+            Icon(
+                imageVector = Icons.Filled.Home,
+                contentDescription = "home",
+            )
+        }, label = { Text(text = "Home") })
+
+        NavigationBarItem(selected = index == 1, onClick = { index = 1 }, icon = {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = "favorite",
+            )
+        }, label = { Text(text = "Favorite") })
+
+        NavigationBarItem(selected = index == 2, onClick = { index = 2 }, icon = {
+            Icon(
+                imageVector = Icons.Filled.Person,
+                contentDescription = "person",
+            )
+        }, label = { Text(text = "Person") })
+    }
 }
