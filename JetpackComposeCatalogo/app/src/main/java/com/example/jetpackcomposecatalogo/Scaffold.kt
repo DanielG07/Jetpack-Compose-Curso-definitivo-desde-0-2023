45fa74ac41f86cf67ad5b22e0ef4af18f96a4a3e
Daniel González <danielgj7@outlook.com>
2024-12-09 15:19:43 -0600

    Component Drawer

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Scaffold.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Scaffold.kt
@@ -1,61 +1,79 @@
 package com.example.jetpackcomposecatalogo
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldExample() {
     val scaffoldHostState = remember { SnackbarHostState() }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
-    Scaffold(
-        snackbarHost = {
-            SnackbarHost(scaffoldHostState)
-        },
-        topBar = {
-            MyTopAppBar {
-                scope.launch { scaffoldHostState.showSnackbar("Has pulsado $it") }
-            }
-        },
-        bottomBar = { MyBottomNavigation() },
-        floatingActionButton = { MyFAB() },
-        floatingActionButtonPosition = FabPosition.Center,
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = { MyDrawer() },
     ) {
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(scaffoldHostState)
+            },
+            topBar = {
+                MyTopAppBar(
+                    onClickIcon = { scope.launch { scaffoldHostState.showSnackbar("Has pulsado $it") } },
+                    onClickMenuIcon = { scope.launch { drawerState.open() } },
+                )
+            },
+            bottomBar = { MyBottomNavigation() },
+            floatingActionButton = { MyFAB() },
+            floatingActionButtonPosition = FabPosition.Center,
+        ) {
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+fun MyTopAppBar(
+    onClickIcon: (String) -> Unit,
+    onClickMenuIcon: () -> Unit,
+) {
     TopAppBar(
         title = { Text(text = "Mi primer toolbar") },
         colors =
@@ -64,8 +82,8 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
                 titleContentColor = Color.White,
             ),
         navigationIcon = {
-            IconButton(onClick = { onClickIcon("Regresar") }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+            IconButton(onClick = { onClickMenuIcon() }) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
             }
         },
         actions = {
@@ -148,5 +166,21 @@ fun MyFAB() {
         contentColor = Color.White,
     ) {
         Icon(imageVector = Icons.Filled.Add, contentDescription = "add")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyDrawer() {
+    ModalDrawerSheet {
+        Text(
+            text = "Primer opción",
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+        NavigationDrawerItem(
+            label = { Text(text = "Drawer Item") },
+            selected = false,
+            onClick = { /*TODO*/ },
+        )
     }
 }
