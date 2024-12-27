5feda7a3c79c1afe70d272631e633c044c917971
Daniel González <danielgj7@outlook.com>
2024-12-27 12:57:33 -0600

    Implement database and repository

--- TodoApp/app/build.gradle.kts
+++ TodoApp/app/build.gradle.kts
@@ -75,6 +75,11 @@ dependencies {
 
     // LiveData
     implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
 
 kapt {
